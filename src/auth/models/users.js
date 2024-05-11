e9998f3c3ffdb5b7881e178c0daa5fb209596291
Brendan <bgmcmullen@comcast.net>
2024-05-11 11:03:20 -0400

    made username not required

--- src/auth/models/users.js
+++ src/auth/models/users.js
@@ -7,7 +7,7 @@ const SECRET = process.env.SECRET || 'secretstring';
 
 const userModel = (sequelize, DataTypes) => {
   const model = sequelize.define('Users', {
-    username: { type: DataTypes.STRING, required: true, unique: true },
+    username: { type: DataTypes.STRING, required: true, unique: false },
     password: { type: DataTypes.STRING, required: true },
     role: { type: DataTypes.ENUM('guest', 'artist', 'vendor', 'admin'), required: true, defaultValue: 'guest'},
     token: {
